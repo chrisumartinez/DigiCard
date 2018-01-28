c3413f4a150547ae52aa5b8f499967c0f6c2c983
jayrave <jayanthan.raveendiran@gmail.com>
2018-01-28 14:39:06 -0800

    better user profile

--- app/src/main/kotlin/xyz/digicard/app/android/ConnectedUserFragment.kt
+++ app/src/main/kotlin/xyz/digicard/app/android/ConnectedUserFragment.kt
@@ -3,9 +3,11 @@ package xyz.digicard.app.android
 import android.os.Bundle
 import android.support.annotation.IdRes
 import android.support.v4.app.Fragment
+import android.support.v4.content.res.ResourcesCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import com.jayrave.falkon.dao.findById
 import xyz.digicard.app.android.models.User
@@ -36,6 +38,14 @@ class ConnectedUserFragment : Fragment() {
         view.setTextOrHide(R.id.email, user.email)
         view.setTextOrHide(R.id.company, user.company)
         view.setTextOrHide(R.id.phone_number, user.phone)
+
+        view.findViewById<ImageView>(R.id.profile_image).setImageDrawable(
+                ResourcesCompat.getDrawable(
+                        context.resources,
+                        user.findProfilePicDrawableResId(),
+                        context.theme
+                )
+        )
     }
 
 
