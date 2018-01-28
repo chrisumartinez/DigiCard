0dcae858e81cf56d31313e80ab98e5d35e2583f7
jayrave <jayanthan.raveendiran@gmail.com>
2018-01-28 15:08:38 -0800

    use splash screen

--- app/src/main/kotlin/xyz/digicard/app/android/StartActivity.kt
+++ app/src/main/kotlin/xyz/digicard/app/android/StartActivity.kt
@@ -1,8 +1,11 @@
 package xyz.digicard.app.android
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import java.lang.ref.WeakReference
 
 class StartActivity : AppCompatActivity() {
 
@@ -10,12 +13,25 @@ class StartActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
 
-        val currentUserId = CurrentUser.getId(this)
-        when (currentUserId) {
-            null -> startActivity(Intent(this, NewActivity::class.java))
-            else -> startActivity(Intent(this, Menu::class.java))
-        }
+        findViewById<View>(R.id.root).postDelayed(
+                ActivityStarter(WeakReference(this)),
+                2000L
+        )
+    }
 
-        finish()
+
+    private class ActivityStarter(private val activityWr: WeakReference<Activity>): Runnable {
+        override fun run() {
+            val activity = activityWr.get()
+            if (activity != null) {
+                val currentUserId = CurrentUser.getId(activity)
+                when (currentUserId) {
+                    null -> activity.startActivity(Intent(activity, NewActivity::class.java))
+                    else -> activity.startActivity(Intent(activity, Menu::class.java))
+                }
+
+                activity.finish()
+            }
+        }
     }
 }
